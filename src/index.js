c0d287e35a72f7ebf89af0de97fbf6aadfba35fb
LeeNux <lc7457@qq.com>
2023-04-03 10:37:44 +0800

    修复引用路径

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export {default as VcImage} from "./component/Image/index.js"
-export {default as VcPcAppLayout} from "@/layout/PcAppLayout/index.js"
+export {default as VcPcAppLayout} from "./layout/PcAppLayout/index.js"
 export {default as FullScrollView} from "./view/FullScrollView/index.js"
 export {default as VcEllipseMask} from "./mask/EllipseMask/index.js"
 export {default as VcLoading} from "./mask/EllipseMask/index.js"
